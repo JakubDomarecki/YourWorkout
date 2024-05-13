4444ba88073f4f69927848b492650c7740f0a4f2
kuba86952@wp.pl <kuba86952@wp.pl>
2024-05-13 15:58:51 +0200

    added working add-button and delete-button

--- js/PhysicalActivity.jsx
+++ js/PhysicalActivity.jsx
@@ -1,45 +1,92 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "../SCSS/main.scss"
 import "../SCSS/componentsScss/physicalActivity.scss"
+import { v4 as uuidv4 } from 'uuid'; // Import funkcji generującej UUID
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const PhysicalActivity = () => {
-    const [date, setDate] = useState();
-    const [exercise, setExercise] = useState();
-    const [series, setSeries] = useState()
-    const [weight, setWeight] = useState();
-    const [repetitions, setRepetitions] = useState();
+    //formularz
+    const [formData, setFormData] = useState({
+        date: '',
+        exercise: '',
+        series: 0,
+        weight: 0,
+        repetitions: 0
+    });
 
-    const handleDateInput = (e) => {
-        const NewDate = e.target.value;
-        setDate(NewDate);
+    //wpisy
+    const [entries, setEntries] = useState([]);
+
+    // pobrane dane z localStorage podczas montowania komponentu
+    useEffect(() => {
+        const storedEntries = localStorage.getItem('entries');
+        if (storedEntries) {
+            setEntries(JSON.parse(storedEntries));
+        }
+    }, []);
+
+
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newEntry = {...formData, id: uuidv4()};
+
+        setEntries(prevState => [...prevState, newEntry])
+
+        localStorage.setItem('entries', JSON.stringify([...entries, newEntry]));
+
+        setFormData({
+            date: '',
+            exercise: '',
+            series: 0,
+            weight: 0,
+            repetitions: 0
+        });
+    };
+
+    const handleDelete = (id) => {
+        const newEntries = entries.filter(entry => entry.id !== id);
+        setEntries(newEntries);
+        localStorage.setItem('entries', JSON.stringify(newEntries))
     }
 
-   return (
+    return (
        <div className="PhysicalActivityBody">
-           <form className="form">
+           <form className="form" onSubmit={handleSubmit}>
                <div className="form__field">
                    <label className="form__label">Date</label>
-                   <input type="date" className="form__input" value={date}/>
+                   <input type="date" className="form__input" value={formData.date} onChange={handleChange} name="date"/>
                </div>
 
                <div className="form__field">
                    <label className="form__label">Exercise</label>
-                   <input type="text" className="form__input"/>
+                   <input type="text" className="form__input" value={formData.exercise} onChange={handleChange} name="exercise"/>
                </div>
 
                <div className="form__field">
                    <label className="form__label">series</label>
-                   <input type="number" className="form__input"/>
+                   <input type="number" className="form__input" value={formData.series} onChange={handleChange} name="series"/>
                </div>
 
                <div className="form__field">
                    <label className="form__label">weight</label>
-                   <input type="number" className="form__input"/>
+                   <input type="number" className="form__input" value={formData.weight} onChange={handleChange} name="weight"/>
                </div>
 
                <div className="form__field">
                    <label className="form__label">the number of repetitions</label>
-                   <input type="number" className="form__input"/>
+                   <input type="number" className="form__input" value={formData.repetitions} onChange={handleChange} name="repetitions"/>
                </div>
                <button type="submit" className="btn--primary">ADD</button>
            </form>
@@ -50,7 +97,7 @@ const PhysicalActivity = () => {
                    <th>Data</th>
                    <th>Exercise</th>
                    <th>series</th>
-                   <th>weight</th>
+                   <th>weight (kg)</th>
                    <th>the number of repetitions</th>
                </tr>
                </thead>
@@ -69,10 +116,25 @@ const PhysicalActivity = () => {
                    <td>180</td>
                    <td>8</td>
                </tr>
+               {entries.map((entry, index) => (
+                   <tr key={entry.id}>
+                       <td>{entry.date}</td>
+                       <td>{entry.exercise}</td>
+                       <td>{entry.series}</td>
+                       <td>{entry.weight}</td>
+                       <td>{entry.repetitions}</td>
+                       <button onClick={() => handleDelete(entry.id)} className="btn--delete">
+                           <FontAwesomeIcon icon={faTrashAlt} style={{color: "red", width: '50px', height: "40px"}}/>
+                       </button>
+                       <button className="btn--edite">
+                           <FontAwesomeIcon icon={faEdit} style={{color: "brown", width: '50px', height: "40px"}}/>
+                       </button>
+                   </tr>
+               ))}
 
                </tbody>
            </table>
        </div>
-   );
+    );
 }
 export default PhysicalActivity;
